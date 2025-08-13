cc3fd614596800f05179352ba28f35730230633f
Michal1898 <73547700+Michal1898@users.noreply.github.com>
2025-08-13 15:32:16 +0200

    NewGame feature imlemented

--- src/Quiz/Main.java
+++ src/Quiz/Main.java
@@ -2,6 +2,7 @@ package Quiz;
 
 import Answers.Answer;
 import Questions.Question;
+
 import java.io.*;
 import java.util.*;
 
@@ -30,99 +31,103 @@ public class Main {
 
         // New quiz starts
         System.out.println("Kviz zacina!");
-        int questionCount = 7;
-        int possiblePoints = 0;
-        int earnedPoints = 0;
-        for (int a = 0; a < questionCount; ++a) {
-            Collections.shuffle(questions);
-            // Select random question
-            Question randomQuestion = questions.get(0);
-            // and remove it from pool
-            questions.remove(0);
-            int randomQuestionHash = randomQuestion.hashCode();
-            ArrayList<Answer> possibleAnswers = new ArrayList<Answer>();
-            for (Answer answer : answers) {
-                if (randomQuestionHash == answer.getHashCode()) {
-                    possibleAnswers.add(answer);
+        final int questionCount = 7;
+
+        do {
+            int possiblePoints = 0;
+            int earnedPoints = 0;
+            for (int a = 0; a < questionCount; ++a) {
+                Collections.shuffle(questions);
+                // Select random question
+                Question randomQuestion = questions.get(0);
+                // and remove it from pool
+                questions.remove(0);
+                int randomQuestionHash = randomQuestion.hashCode();
+                ArrayList<Answer> possibleAnswers = new ArrayList<Answer>();
+                for (Answer answer : answers) {
+                    if (randomQuestionHash == answer.getHashCode()) {
+                        possibleAnswers.add(answer);
+                    }
+
                 }
+                // random shuffle of answer order
+                Collections.shuffle(possibleAnswers);
+                System.out.println("\n");
+                System.out.println("Otazka c: " + (a + 1));
+                System.out.print(randomQuestion);
 
-            }
-            // random shuffle of answer order
-            Collections.shuffle(possibleAnswers);
-            System.out.println("\n");
-            System.out.println("Otazka c: " + (a + 1));
-            System.out.print(randomQuestion);
-
-            for (Answer answer : possibleAnswers) {
-                int paragraphIndex = possibleAnswers.indexOf(answer);
-                String paragraphName = paragraphNameCoder.get(paragraphIndex);
-                System.out.print(paragraphName + " ) " + answer);
-            }
-            Scanner userInput = new Scanner(System.in);
-            if (randomQuestion.getType().equals("Single choice")) {
-                // Evalution of SingleChoice answer
-                possiblePoints++;
-                String yourAnswer;
-                do{
-                    System.out.println("Oznac spravnou odpoved: ");
-                    yourAnswer = userInput.nextLine().toLowerCase().trim();
-                } while(yourAnswer.length() != 1 && !paragraphNameCoder.contains(yourAnswer) );
-
-                //earnedPoints
-                int answerIndex = paragraphNameCoder.indexOf(yourAnswer);
-                Answer markedAnswer = possibleAnswers.get(answerIndex);
-
-                System.out.println("Tvoje odpoved: \n" + markedAnswer);
-                if (markedAnswer.getAnswerCorrect()) {
-                    earnedPoints++;
-                    System.out.println("Uhodl jsi. Gratuluji. ");
-                } else {
-                    System.out.println("Chybna odpoved. Cha Cha Chaa!");
-                }
-
-            } else {
-                // evalution of multipleChoice answer
-                int thisQuestionPossiblePoints = 0;
-                int thisQuestionEarnedPoints = 0;
                 for (Answer answer : possibleAnswers) {
-
-                    if (answer.getAnswerCorrect()) {
-                        thisQuestionPossiblePoints++;
-                    }
+                    int paragraphIndex = possibleAnswers.indexOf(answer);
+                    String paragraphName = paragraphNameCoder.get(paragraphIndex);
+                    System.out.print(paragraphName + " ) " + answer);
                 }
-                possiblePoints += thisQuestionPossiblePoints;
+                Scanner userInput = new Scanner(System.in);
+                if (randomQuestion.getType().equals("Single choice")) {
+                    // Evalution of SingleChoice answer
+                    possiblePoints++;
+                    String yourAnswer;
+                    do {
+                        System.out.println("Oznac spravnou odpoved: ");
+                        yourAnswer = userInput.nextLine().toLowerCase().trim();
+                    } while (yourAnswer.length() != 1 && !paragraphNameCoder.contains(yourAnswer));
 
-                System.out.println("Zadej svoji odpoved (mozna je vice odpovedi spravnych)");
-                String yourAnswer = userInput.nextLine().toLowerCase().trim();
-                String[] yourAnswers = yourAnswer.split(" ");
-                for (String oneAnswer : yourAnswers) {
-                    int answerIndex = paragraphNameCoder.indexOf(oneAnswer);
+                    //earnedPoints
+                    int answerIndex = paragraphNameCoder.indexOf(yourAnswer);
                     Answer markedAnswer = possibleAnswers.get(answerIndex);
-                    if (markedAnswer.getAnswerCorrect()) {
-                        thisQuestionEarnedPoints++;
-                    } else {
-                        thisQuestionEarnedPoints -= 2;
-                    }
-                }
-                earnedPoints += thisQuestionEarnedPoints;
-                if (thisQuestionEarnedPoints == thisQuestionPossiblePoints) {
-                    System.out.println("Otazku jsi uspesne vyresil. Gratuluji");
-                } else {
-                    System.out.println("Chybna nebo neuplna odpoved!");
-                }
 
+                    System.out.println("Tvoje odpoved: \n" + markedAnswer);
+                    if (markedAnswer.getAnswerCorrect()) {
+                        earnedPoints++;
+                        System.out.println("Uhodl jsi. Gratuluji. ");
+                    } else {
+                        System.out.println("Chybna odpoved. Cha Cha Chaa!");
+                    }
+
+                } else {
+                    // evalution of multipleChoice answer
+                    int thisQuestionPossiblePoints = 0;
+                    int thisQuestionEarnedPoints = 0;
+                    for (Answer answer : possibleAnswers) {
+
+                        if (answer.getAnswerCorrect()) {
+                            thisQuestionPossiblePoints++;
+                        }
+                    }
+                    possiblePoints += thisQuestionPossiblePoints;
+
+                    System.out.println("Zadej svoji odpoved (mozna je vice odpovedi spravnych)");
+                    String yourAnswer = userInput.nextLine().toLowerCase().trim();
+                    String[] yourAnswers = yourAnswer.split(" ");
+                    for (String oneAnswer : yourAnswers) {
+                        int answerIndex = paragraphNameCoder.indexOf(oneAnswer);
+                        Answer markedAnswer = possibleAnswers.get(answerIndex);
+                        if (markedAnswer.getAnswerCorrect()) {
+                            thisQuestionEarnedPoints++;
+                        } else {
+                            thisQuestionEarnedPoints -= 2;
+                        }
+                    }
+                    earnedPoints += thisQuestionEarnedPoints;
+                    if (thisQuestionEarnedPoints == thisQuestionPossiblePoints) {
+                        System.out.println("Otazku jsi uspesne vyresil. Gratuluji");
+                    } else {
+                        System.out.println("Chybna nebo neuplna odpoved!");
+                    }
+
+                }
+                System.out.println("Prubezne skore:");
+                System.out.println("Aktualne mas " + earnedPoints + " z " + possiblePoints + " moznych");
+                if (possiblePoints != 0) {
+                    System.out.println("To je " + (100 * earnedPoints / possiblePoints) + " %.");
+                }
             }
-            System.out.println("Prubezne skore:");
-            System.out.println("Aktualne mas " + earnedPoints + " z " + possiblePoints + " moznych");
+            System.out.println("Konec testu:");
+            System.out.println("Celkove jsi ziskal " + earnedPoints + " z " + possiblePoints + " moznych");
             if (possiblePoints != 0) {
                 System.out.println("To je " + (100 * earnedPoints / possiblePoints) + " %.");
             }
-        }
-        System.out.println("Konec testu:");
-        System.out.println("Celkove jsi ziskal " + earnedPoints + " z " + possiblePoints + " moznych");
-        if (possiblePoints != 0) {
-            System.out.println("To je " + (100 * earnedPoints / possiblePoints) + " %.");
-        }
+        } while (anotherGame());
+
 
     }
 
@@ -210,4 +215,41 @@ public class Main {
             System.out.println("Something went wrong!");
         }
     }
+
+    public static Boolean anotherGame() {
+        final String ANSI_RESET = "\u001B[0m";
+        final String ANSI_BLACK = "\u001B[30m";
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_GREEN = "\u001B[32m";
+        final String ANSI_YELLOW = "\u001B[33m";
+        final String ANSI_BLUE = "\u001B[34m";
+        final String ANSI_PURPLE = "\u001B[35m";
+        final String ANSI_CYAN = "\u001B[36m";
+        final String ANSI_WHITE = "\u001B[37m";
+
+        Scanner again = new Scanner(System.in);
+        String yesNo;
+        while (true) {
+            try {
+                System.out.println("Nova hra?");
+                yesNo = again.nextLine().toLowerCase();
+                if ((yesNo.equals("ano")) || (yesNo.equals("ne"))) {
+                    break;
+                } else {
+                    System.out.println(ANSI_RED + "Odpovez ano, nebo ne!" + ANSI_RESET);
+                    again.nextLine();
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input!");
+                System.out.println("Odpovez ano, nebo ne!");
+            }
+
+        }
+        if (yesNo.equals("ano")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
